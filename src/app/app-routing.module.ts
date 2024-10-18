b01ad10e25bc99eb62d70df178e8ee8528e3715c
FranciscoSilva <franc.silvaj@duocuc.cl>
2024-10-17 23:24:19 -0300

    Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AuthGuardService } from './auth-guard.service';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
     canActivate:[AuthGuardService]
   },
   {
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Observable } from 'rxjs';
 
@@ -8,9 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(private authService:AuthService) { }
+  constructor(private authService:AuthService, private router:Router) { }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    throw new Error('Method not implemented.');
     if(this.authService.isAuthenticated()){
       
       return true;
@@ -18,5 +17,6 @@ export class AuthGuardService implements CanActivate {
       this.router.navigate(['/login']);
       return false;
     }
+    
   }
 }
